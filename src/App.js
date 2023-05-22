2e63b69c5930bacebfd9c29deeee2e4a9d887954
Junkyu Park <junqwoopark@gmail.com>
2023-05-22 15:08:42 +0900

    study: Coin Tracker challenge

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
 
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
+  const onAmountChange = (event) => {
+    setAmount(event.target.value);
   };
+
+  const onSelectCoin = (event) => {
+    event.preventDefault();
+    setValue(event.target.value);
+  };
+
+  function fetchCoins() {
+    fetch("https://api.coinpaprika.com/v1/tickers?quotes=KRW")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }
+
+  useEffect(fetchCoins, []);
+
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins!</h1>
+      {loading ? <div>loading...</div> : null}
+      {!loading ? (
+        <select onChange={onSelectCoin}>
+          <option>Select Coin</option>
+          {coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.KRW.price}>
+              {coin.name} ({coin.symbol}) {coin.quotes.KRW.price} KRW
+            </option>
+          ))}
+        </select>
+      ) : null}
+      <h2>Please enter the amount</h2>
+      <div>
         <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
+          onChange={onAmountChange}
+          value={amount}
+          type="number"
+          placeholder="KRW"
         />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+        <h2>You can get {amount / value} coins</h2>
+      </div>
+      <div></div>
     </div>
   );
 }
