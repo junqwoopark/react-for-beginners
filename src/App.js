f3695d315e0a42f09611568c2e687d300327e500
Junkyu Park <junqwoopark@gmail.com>
2023-05-22 13:10:31 +0900

    study: useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  // state가 변경되면, 모든 컴포넌트가 다시 렌더링된다.
+  // 반복하게 되면, 성능이 저하된다.
+  console.log("I run all the time.");
+
+  // useEffect를 사용하면, state가 변경되어도, 해당 함수는 한번만 실행된다.
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' || 'counter' changes.");
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text="Continue" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
